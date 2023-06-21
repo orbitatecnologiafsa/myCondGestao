69007f0484f549c97fb4a14e002079aa6bca06c4
orbitatecnologiafsa <orbitatecnologiafsa@gmail.com>
2023-06-21 17:56:03 -0300

    up

--- resources/views/tamplate/layout/main.blade.php
+++ resources/views/tamplate/layout/main.blade.php
@@ -19,7 +19,7 @@
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <link href="{{ asset('css/nucleo-svg.css') }}" rel="stylesheet" />
     <!-- CSS Files -->
-    <link id="pagestyle" href="{{ asset('css/argon-dashboard.css?v=2.0.4') }}" rel="stylesheet" />
+    <link id="pagestyle" href="{{ asset('css/argon-dashboard.css') }}" rel="stylesheet" />
     <style>
         .async-hide {
             opacity: 0 !important
