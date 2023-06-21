29edf35f29a7b324daf07d85f5f29cfdcfab05c4
orbitatecnologiafsa <orbitatecnologiafsa@gmail.com>
2023-06-21 15:57:51 -0300

    up

--- resources/views/tamplate/sidbars/sidbar-adm.blade.php
+++ resources/views/tamplate/sidbars/sidbar-adm.blade.php
@@ -57,6 +57,15 @@
                         <span class="nav-link-text ms-1">Cadastrar Funcionario</span>
                     </a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link @yield('ativo-cadastrar-port')" href="{{route('adm-cadastrar-funcionario')}}">
+                        <div
+                            class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="ni ni-single-02 text-primary text-sm opacity-10"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">Cadastrar Visitante</span>
+                    </a>
+                </li>
             </li>
             <li class="nav-item">
                 <a class="nav-link @yield('ativo-caixa') " href="">
